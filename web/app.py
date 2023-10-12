21bc25b991a8ca2396d5d4b367bab6a2ec2afffb
Donghee Park <dongheepark@gmail.com>
2023-10-12 14:18:00 +0900

    Allow CORS for static files

--- web/app.py
+++ web/app.py
@@ -7,5 +7,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.after_request
+def after_request(response):
+    header = response.headers
+    header['Access-Control-Allow-Origin'] = '*'
+    return response
+
 if __name__ == '__main__':
       app.run(host='0.0.0.0', port=5053)
